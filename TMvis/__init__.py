6a96bfe95f3f1a6b64a6d042c60889364ef875f3
C-Marquet <73125710+C-Marquet@users.noreply.github.com>
2022-07-29 17:04:50 +0200

    Create __init__.py

---
+++ TMvis/__init__.py
@@ -0,0 +1,7 @@
+__author__ = "Anastasia G., Celine M."
+__copyright__ = "Copyright 2022, Rostlab"
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = ["Anastasia G., Celine M."]
+__email__ = ""
+__status__ = "Production"
